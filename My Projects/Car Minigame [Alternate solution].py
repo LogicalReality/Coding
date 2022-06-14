2636939a51cd79f4122f54b6f72e5ff6d09c1702
LogicalReality <carlos26ch@gmail.com>
2022-06-13 23:58:44 -0400

    Alternate Solution Car engine

--- My Projects/Car Minigame [Alternate solution].py
+++ My Projects/Car Minigame [Alternate solution].py
@@ -1,23 +1,23 @@
-
 # This is a simple car engine game.
 
+#Alternate solution for this minigame.
 while True:
-    command = input('> Enter a command: ')
+    command = input('> Enter a command: ').lower()
     try:
-        if 'help' in command.lower():
+        if command == 'help':
             # This print function could be better, the indentation that appears of using triple quotes made me come up with this solution.
             print(
                 '\n---> Enter one of the following commands: \n\n start - to start the car \n stop - to stop the car \n exit - to exit game\n')       
 
-        elif 'start' in command.lower():
+        elif command == 'start':
             print('Car started... Ready to go!\n')
         
-        elif 'stop' in command.lower():
+        elif command == 'stop':
             print('Car stopped.\n')
         
-        elif 'exit' in command.lower():
-            break 
-
+        elif command == 'exit':
+            break
+        
         else:
             print("I don't understand that...\n")
     except ValueError:
